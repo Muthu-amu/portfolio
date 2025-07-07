f44c36bc669a33e0e75997542bab8708ef927873
Muthu-amu <muthu.amu.mp@gmail.com>
2025-07-07 16:37:14 +0530

    notepad and NavbarComponent updated

--- src/components/layout/NavbarComponent.jsx
+++ src/components/layout/NavbarComponent.jsx
@@ -1,5 +1,5 @@
 import { Navbar } from 'flowbite-react';
-console.log('Navbar from flowbite:', Navbar);
+
 const NavbarComponent = () => {
   return (
     <Navbar fluid rounded className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
@@ -8,22 +8,15 @@ const NavbarComponent = () => {
           DummyLogo
         </span>
       </Navbar.Brand>
+
+      <Navbar.Toggle />
+
       <Navbar.Collapse className="justify-center">
-        <Navbar.Link href="#about" className="text-gray-700 hover:text-blue-600">
-          About
-        </Navbar.Link>
-        <Navbar.Link href="#skills" className="text-gray-700 hover:text-blue-600">
-          Skills
-        </Navbar.Link>
-        <Navbar.Link href="#visionary" className="text-gray-700 hover:text-blue-600">
-          Visionary
-        </Navbar.Link>
-        <Navbar.Link href="#projects" className="text-gray-700 hover:text-blue-600">
-          Projects
-        </Navbar.Link>
-        <Navbar.Link href="#experience" className="text-gray-700 hover:text-blue-600">
-          Experience
-        </Navbar.Link>
+        <Navbar.Link href="#about">About</Navbar.Link>
+        <Navbar.Link href="#skills">Skills</Navbar.Link>
+        <Navbar.Link href="#visionary">Visionary</Navbar.Link>
+        <Navbar.Link href="#projects">Projects</Navbar.Link>
+        <Navbar.Link href="#experience">Experience</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
